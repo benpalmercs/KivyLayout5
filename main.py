83b195de2bb340ef5eebae4beaf7e283c902e003
benpalmercs <benpalmerthrowaway@outlook.com>
2023-09-26 10:05:27 -0500

    GridLayout and ScrollView

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 class MainWidget(Widget):
@@ -6,5 +10,18 @@ class MainWidget(Widget):
 
 class WidgetLayoutApp(App):
     pass
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "lr-tb"
+        for letter in "abcdefghijklmnopqrstuvwxyz0123456789":
+            b = Button(text="Button "+letter, size_hint=(.2,.2))
+            self.add_widget(b)
+class BoxLayoutExample(BoxLayout):
+    pass
+class GridLayoutExample(GridLayout):
+    pass
+
 
 WidgetLayoutApp().run()
