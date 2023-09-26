56c9d766a5b41d6021401b9551ee5df35e3910e9
benpalmercs <benpalmerthrowaway@outlook.com>
2023-09-26 10:25:43 -0500

    not much

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
+class LayoutAssignment(BoxLayout):
+    pass
 class MainWidget(Widget):
     pass
 
 class WidgetLayoutApp(App):
     pass
 
-class StackLayoutExample(StackLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "lr-tb"
-        for letter in "abcdefghijklmnopqrstuvwxyz0123456789":
-            b = Button(text="Button "+letter, size_hint=(.2,.2))
-            self.add_widget(b)
+#class StackLayoutExample(StackLayout):
+    #def __init__(self, **kwargs):
+        #super().__init__(**kwargs)
+        #self.orientation = "lr-tb"
+        #for letter in "abcdefghijklmnopqrstuvwxyz0123456789":
+            #b = Button(text="Button "+letter, size_hint=(.2,.2))
+            #self.add_widget(b)
 class BoxLayoutExample(BoxLayout):
     pass
 class GridLayoutExample(GridLayout):
