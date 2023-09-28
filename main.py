b91b99526001e3ae43ec35edad44a49ee668628b
benpalmercs <benpalmerthrowaway@outlook.com>
2023-09-28 10:10:00 -0500

    not much

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -7,23 +10,8 @@ from kivy.uix.widget import Widget
 
 class LayoutAssignment(BoxLayout):
     pass
-class MainWidget(Widget):
-    pass
 
 class WidgetLayoutApp(App):
     pass
-
-#class StackLayoutExample(StackLayout):
-    #def __init__(self, **kwargs):
-        #super().__init__(**kwargs)
-        #self.orientation = "lr-tb"
-        #for letter in "abcdefghijklmnopqrstuvwxyz0123456789":
-            #b = Button(text="Button "+letter, size_hint=(.2,.2))
-            #self.add_widget(b)
-class BoxLayoutExample(BoxLayout):
-    pass
-class GridLayoutExample(GridLayout):
-    pass
-
 
 WidgetLayoutApp().run()
